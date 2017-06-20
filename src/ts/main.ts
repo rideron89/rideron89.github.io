17e1f10ce4dfe5ab15ca363a3fb096d8a42ab976
Ron Rider <rrider@planitagency.com>
2017-06-20 16:44:00 -0400

    initial webpack files and config

---
+++ src/ts/main.ts
@@ -0,0 +1,5 @@
+/// <reference path="../../node_modules/@types/node/index.d.ts" />
+
+const style = require('../scss/main.scss');
+
+console.log('hello');
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'ts', 'main.ts'),
+
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        loaders: [
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract('css-loader!sass-loader') },
+            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' }
+        ]
+    },
+
+    resolve: {
+        extensions: [ '.scss', '.ts' ]
+    },
+
+    plugins: [
+        new ExtractTextPlugin({ filename: 'bundle.css', allChunks: true })
+    ]
+};
