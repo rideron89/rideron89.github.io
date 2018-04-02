7476e5352ddb3169fa8352f6f7248e25e4d56616
Ron Rider <rideron89@gmail.com>
2018-04-02 00:42:19 -0400

    add mostly complete first version

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,61 @@
 /// <reference path="../../node_modules/@types/node/index.d.ts" />
 
-const style = require('../scss/main.scss');
+const style = require("../scss/app.scss");
 
-console.log('hello');
+const $ = function (selector) {
+    let $elems: Element[];
+
+
+    if (typeof selector === "string") {
+        $elems = Array.prototype.slice.call(document.querySelectorAll(selector));
+    } else if (selector instanceof Element) {
+        $elems = [selector];
+    } else {
+        $elems = selector;
+    }
+
+    return {
+        get: (index: number): Element => {
+            return $elems[index];
+        },
+
+        on: (event, callback) => {
+            $elems.forEach(($elem) => {
+                $elem.addEventListener(event, callback);
+            });
+        },
+
+        scrollTo: () => {
+            const target = (<HTMLElement>$elems[0]).offsetTop - 50;
+
+            window.scrollTo({
+                    behavior: "smooth",
+                    left: 0,
+                    top: target,
+            });
+        }
+    };
+};
+
+/*****************************************************************************/
+
+const $headerNavLinks = $("header nav a");
+
+$headerNavLinks.on("click", (ev) => {
+    ev.preventDefault();
+
+    const target = ev.currentTarget.getAttribute("href");
+    const $target = $(target);
+
+    $(target).scrollTo();
+});
+
+const $radioButtons = $("form .radio-button");
+
+$radioButtons.on("keypress", (ev) => {
+    if (ev.keyCode === 13) {
+        const $input = ev.currentTarget.querySelector("input");
+
+        $input.dispatchEvent(new MouseEvent("click"));
+    }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    entry: path.join(__dirname, 'src', 'ts', 'main.ts'),
+    entry: path.join(__dirname, "src", "ts", "main.ts"),
 
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path: path.join(__dirname, "dist"),
+        filename: "bundle.js"
     },
 
     module: {
         loaders: [
-            { test: /\.scss$/, loader: ExtractTextPlugin.extract('css-loader!sass-loader') },
-            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' }
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract("css-loader?url=false!sass-loader") },
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
         ]
     },
 
     resolve: {
-        extensions: [ '.scss', '.ts' ]
+        extensions: [ ".scss", ".ts" ]
     },
 
     plugins: [
-        new ExtractTextPlugin({ filename: 'bundle.css', allChunks: true })
+        new ExtractTextPlugin({ filename: "bundle.css", allChunks: true })
     ]
 };
